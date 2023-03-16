87fd25b560d65200f382a8b2888ffb8eb6343d4f
Aaron <22418751+ajvsol@users.noreply.github.com>
2023-03-16 16:08:59 +0000

    fix: hops ingredients no longer renders duplicates

--- pages/beers/[id].tsx
+++ pages/beers/[id].tsx
@@ -7,12 +7,12 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  console.log(`getServerSideProps`);
+  //console.log(`getServerSideProps`);
 
   const res = await fetch(`https://api.punkapi.com/v2/beers/${params?.id}`);
 
   const data = await res.json();
-  console.log(data);
+  //console.log(data);
 
   return {
     props: {
@@ -23,33 +23,26 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 
 export default function BeerPage({ data }: any) {
   const router = useRouter();
-  
+
   const ingred = data[0].ingredients;
 
+  let page = data[0].id;
 
-
-  let page = data[0].id
   const [pageNumber, setPageNumber] = useState(page);
-  console.log("this is the page number",pageNumber)
-  // useEffect(() => {
-  //   const setCurrentPageNumber = async () => {
-  //     setPageNumber(page);
-  //     console.log("beer", page);
-  //   };
-  //   setCurrentPageNumber();
-  // }, []);
+
+  const renderedHopNames = new Set();
 
   const redirectPreviousPage = () => {
     if (pageNumber > 1) {
-      setPageNumber((prevPageNumber:number) => prevPageNumber - 1);
+      setPageNumber((prevPageNumber: number) => prevPageNumber - 1);
       router.push(`/beers/${pageNumber - 1}`);
     } else {
       router.push(`/`);
     }
   };
-  
+
   const redirectNextPage = () => {
-    setPageNumber((prevPageNumber:number) => prevPageNumber + 1);
+    setPageNumber((prevPageNumber: number) => prevPageNumber + 1);
     router.push(`/beers/${pageNumber + 1}`);
   };
 
@@ -57,9 +50,9 @@ export default function BeerPage({ data }: any) {
     <div className="bg-cyan-700 h-full w-full">
       <Hero heading={data[0].name} message={data[0].tagline} />
       <div className="flex justify-center p-4">
-      <Link href="/">
-        <button
-          className="border-2
+        <Link href="/">
+          <button
+            className="border-2
         bg-cyan-200
         box-border
         h-10
@@ -73,10 +66,10 @@ export default function BeerPage({ data }: any) {
         hover:bg-emerald-300 
         hover:border-emerald-300
         hover:text-slate-900 "
-        >
-          HOME
-        </button>
-      </Link>
+          >
+            HOME
+          </button>
+        </Link>
       </div>
       <div className="flex flex-col justify-center items-center ">
         <div
@@ -85,20 +78,28 @@ export default function BeerPage({ data }: any) {
       shadow-cyan-900"
         >
           <div className="m-auto pb-4">
-          
-            <Image src={data[0].image_url} alt="image" width={130} height={130} />
-            </div>
-          <h1 className="text-3xl text-slate-800 font-bold text-center">{data[0].name}</h1>
+            <Image
+              src={data[0].image_url}
+              alt="image"
+              width={130}
+              height={130}
+            />
+          </div>
+          <h1 className="text-3xl text-slate-800 font-bold text-center">
+            {data[0].name}
+          </h1>
           <h2 className="text-md text-slate-900 ">"{data[0].tagline}"</h2>
-          <h3 className="text-2xl text-cyan-800 ">
-            {data[0].abv}% ABV
-          </h3>
+          <h3 className="text-2xl text-cyan-800 ">{data[0].abv}% ABV</h3>
           <span className="h-1 w-full bg-cyan-700 lg:w-1/3"></span>
           <p className="text-lg text-slate-900 ">{data[0].description} </p>
           <h3 className="text-2xl text-cyan-800 ">Food Pairings:</h3>
           <span className="h-1 w-full bg-cyan-700 lg:w-1/3"></span>
           {data[0].food_pairing.map((food: any) => {
-            return <p className="text-md text-slate-900 " key={food}>{food}</p>;
+            return (
+              <p className="text-md text-slate-900 " key={food}>
+                {food}
+              </p>
+            );
           })}
           <h3 className="text-2xl  text-cyan-800  text-left">Ingredients:</h3>
           <span className="h-1 w-full bg-cyan-700 lg:w-1/3"></span>
@@ -111,22 +112,31 @@ export default function BeerPage({ data }: any) {
             ))}
           </ul>
           <h4 className="text-xl  text-cyan-800   text-left">Hops:</h4>
-          
+
           <ul>
-            {ingred.hops.map((hop: any) => (
-              <li className="text-md text-slate-900 text-left " key={hop.name}>
-                {hop.name}
-              </li>
-            ))}
+            {ingred.hops.map((hop: any) => {
+              if (!renderedHopNames.has(hop.name)) {
+                renderedHopNames.add(hop.name);
+                return (
+                  <li
+                    className="text-md text-slate-900 text-left "
+                    key={hop.name}
+                  >
+                    {hop.name}
+                  </li>
+                );
+              }
+              return null;
+            })}
           </ul>
           <h4 className="text-xl  text-cyan-800   text-left">Yeast:</h4>
-        
+
           <p className="text-md text-slate-900 text-left">{ingred.yeast}</p>
         </div>
         <div className=" flex p-5 gap-5">
-        <button
-          onClick={redirectPreviousPage}
-          className="border-2
+          <button
+            onClick={redirectPreviousPage}
+            className="border-2
         bg-cyan-200
         box-border
         h-10
@@ -141,12 +151,12 @@ export default function BeerPage({ data }: any) {
         hover:border-emerald-300
         hover:text-slate-900 
         "
-        >
-          PREV
-        </button>
-        <button
-          onClick={redirectNextPage}
-          className="border-2
+          >
+            PREV
+          </button>
+          <button
+            onClick={redirectNextPage}
+            className="border-2
         bg-cyan-200
         box-border
         h-10
@@ -160,10 +170,10 @@ export default function BeerPage({ data }: any) {
         hover:bg-emerald-300 
         hover:border-emerald-300
         hover:text-slate-900 "
-        >
-          NEXT
+          >
+            NEXT
           </button>
-          </div>
+        </div>
       </div>
     </div>
   );
