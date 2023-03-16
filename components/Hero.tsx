d012758f1a531e2b54d02ee19908543f7899fb57
OmertaDevs <omertar6@outlook.com>
2023-03-16 13:59:40 +0000

    built the functionality for the next page and prev page buttons for the individual beer pages. Also set up some conditional logic to redirect to home if trying to navigate to a beer page that doesnt exist.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,8 +8,8 @@ function Hero({heading,message}:HeroProps) {
   return (
     <>
       <div className="absolute top-0 left-0 right-0 bottom-0 h-128 bg-black/70 z-[2]"/>
-      <div className=" flex flex-col items-center justify-center h-128 mb-12 bg-fixed bg-center bg-cover custom-img">
-        <h1 className="text-6xl text-cyan-400 z-[3]">{heading}</h1>
+      <div className=" flex flex-col items-center justify-center h-128 mb-4 bg-fixed bg-center bg-cover custom-img">
+        <h1 className="text-5xl text-cyan-400 z-[3]">{heading}</h1>
         <p className="text-2xl text-slate-50 z-[3]">{message}</p>
       </div>
     </>
--- pages/beers/[id].tsx
+++ pages/beers/[id].tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Beer } from "@/types/types";
 import Hero from "@/components/Hero";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
@@ -23,22 +23,61 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 
 export default function BeerPage({ data }: any) {
   const router = useRouter();
-  const { asPath } = router;
-  //console.log(`asPath`, asPath);
-
+  
   const ingred = data[0].ingredients;
-  console.dir(`data`, data);
 
-  const [pageNumber, setPageNumber] = useState();
-  //let previousBeerSlug = `/beers/${pageNumber - 1}`;
-  //let nextBeerSlug = `/beers/${pageNumber + 1}`;
+
+
+  let page = data[0].id
+  const [pageNumber, setPageNumber] = useState(page);
+  console.log("this is the page number",pageNumber)
+  // useEffect(() => {
+  //   const setCurrentPageNumber = async () => {
+  //     setPageNumber(page);
+  //     console.log("beer", page);
+  //   };
+  //   setCurrentPageNumber();
+  // }, []);
+
+  const redirectPreviousPage = () => {
+    if (pageNumber > 1) {
+      setPageNumber((prevPageNumber:number) => prevPageNumber - 1);
+      router.push(`/beers/${pageNumber - 1}`);
+    } else {
+      router.push(`/`);
+    }
+  };
+  
+  const redirectNextPage = () => {
+    setPageNumber((prevPageNumber:number) => prevPageNumber + 1);
+    router.push(`/beers/${pageNumber + 1}`);
+  };
 
   return (
     <div className="bg-cyan-700 h-full w-full">
       <Hero heading={data[0].name} message={data[0].tagline} />
+      <div className="flex justify-center p-4">
       <Link href="/">
-        <button>Back to home</button>
+        <button
+          className="border-2
+        bg-cyan-200
+        box-border
+        h-10
+        w-32
+        border-cyan-200
+        text-slate-900
+        rounded-md
+        shadow-md
+        shadow-cyan-900
+        z-10
+        hover:bg-emerald-300 
+        hover:border-emerald-300
+        hover:text-slate-900 "
+        >
+          Back to home
+        </button>
       </Link>
+      </div>
       <div className="flex flex-col justify-center items-center ">
         <div
           className="bg-cyan-200 h-full w-128 flex flex-col justify-center items-center p-10 gap-3  rounded-3xl
@@ -76,12 +115,47 @@ export default function BeerPage({ data }: any) {
           </ul>
           <p>Yeast: {ingred.yeast}</p>
         </div>
-        <Link href={previousBeerSlug}>
-          <button>Previous beer</button>
-        </Link>
-        <Link href={nextBeerSlug}>
-          <button>Next beer</button>
-        </Link>
+        <div className=" flex p-5 gap-5">
+        <button
+          onClick={redirectPreviousPage}
+          className="border-2
+        bg-cyan-200
+        box-border
+        h-10
+        w-32
+        border-cyan-200
+        text-slate-900
+        rounded-md
+        shadow-md
+        shadow-cyan-900
+        z-10
+        hover:bg-emerald-300 
+        hover:border-emerald-300
+        hover:text-slate-900 
+        "
+        >
+          Previous beer
+        </button>
+        <button
+          onClick={redirectNextPage}
+          className="border-2
+        bg-cyan-200
+        box-border
+        h-10
+        w-32
+        border-cyan-200
+        text-slate-900
+        rounded-md
+        shadow-md
+        shadow-cyan-900
+        z-10
+        hover:bg-emerald-300 
+        hover:border-emerald-300
+        hover:text-slate-900 "
+        >
+          Next beer
+          </button>
+          </div>
       </div>
     </div>
   );
